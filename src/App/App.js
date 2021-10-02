301353c5ec4b0f744beb666ce71277a7a6a94d45
pandoragear <andy.dang.it@gmail.com>
2021-10-01 21:49:49 -0500

    if gamestate equal to menu then render MainMenu component

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import MainMenu from "../Components/MainMenu";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
-  const [gameState, setGameState] = useState('menu');
-  
+  const [gameState, setGameState] = useState("menu");
 
   return (
- <h2>test</h2> 
-  )
+    <div className="App">
+      <h2>test</h2>
+
+      {gameState === "menu" && <MainMenu />}
+    </div>
+  );
 }
 
 export default App;

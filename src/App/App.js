84f8c717c9af0fe813c42c686800a46fd64b2108
pandoragear <andy.dang.it@gmail.com>
2021-10-04 19:20:13 -0500

    add useContext

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import MainMenu from "../Components/MainMenu";
-import Quiz from '../Components/Quiz'
-import EndScreen from '../Components/EndScreen'
+import Quiz from "../Components/Quiz";
+import EndScreen from "../Components/EndScreen";
+
+import { QuizContext } from "../Helpers/Contexts";
 
 import "./App.css";
 
@@ -11,10 +13,11 @@ function App() {
   return (
     <div className="App">
       <h2>test</h2>
-
-      {gameState === "menu" && <MainMenu />}
-      {gameState === "quiz" && <Quiz />}
-      {gameState === "endScreen" && <EndScreen />}
+      <QuizContext.Provider value= {{gameState, setGameState}}>
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
     </div>
   );
 }
